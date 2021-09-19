7987e8ad13816f6404f3ecc6703fd9de30d69483
8kdesign <that8kguy@gmail.com>
2021-09-19 16:03:10 +0800

    Disabled permission checker

--- src/App.js
+++ src/App.js
@@ -16,45 +16,45 @@ import ShareAdapter from "./Screens/Buyer/Share/ShareAdapter";
 import ImageSplashScreen from "./assets/Splashscreen_Lite.png";
 
 export default function App() {
-	const [hasPermission, setPermission] = useState(false);
+	// const [hasPermission, setPermission] = useState(false);
 
-	navigator.permissions.query({ name: "geolocation" }).then((result) => {
-		setPermission(result.state == "granted");
-	});
+	// navigator.permissions.query({ name: "geolocation" }).then((result) => {
+	// 	setPermission(result.state == "granted");
+	// });
 
-	if (!hasPermission) {
-		return (
-			<div className="App">
-				<img
-					src={ImageSplashScreen}
-					className="Image__request-background"
-				/>
-				<div className="Container__footer" style={{ borderWidth: 0 }}>
-					<div className="Buffer__20px" />
-					<div className="Container__large-screen-optimize Container__horizontal-padding-20px">
-						<div
-							className="Toggle__large--primary"
-							onClick={() =>
-								navigator.geolocation.getCurrentPosition(
-									() => {
-										setPermission(true);
-									},
-									() => {
-										setPermission(false);
-									}
-								)
-							}
-						>
-							<p className="Text__medium--light">
-								Enable Location
-							</p>
-						</div>
-					</div>
-					<div className="Buffer__20px" />
-				</div>
-			</div>
-		);
-	}
+	// if (!hasPermission) {
+	// 	return (
+	// 		<div className="App">
+	// 			<img
+	// 				src={ImageSplashScreen}
+	// 				className="Image__request-background"
+	// 			/>
+	// 			<div className="Container__footer" style={{ borderWidth: 0 }}>
+	// 				<div className="Buffer__20px" />
+	// 				<div className="Container__large-screen-optimize Container__horizontal-padding-20px">
+	// 					<div
+	// 						className="Toggle__large--primary"
+	// 						onClick={() =>
+	// 							navigator.geolocation.getCurrentPosition(
+	// 								() => {
+	// 									setPermission(true);
+	// 								},
+	// 								() => {
+	// 									setPermission(false);
+	// 								}
+	// 							)
+	// 						}
+	// 					>
+	// 						<p className="Text__medium--light">
+	// 							Enable Location
+	// 						</p>
+	// 					</div>
+	// 				</div>
+	// 				<div className="Buffer__20px" />
+	// 			</div>
+	// 		</div>
+	// 	);
+	// }
 
 	return (
 		<ThemeProvider theme={theme}>
