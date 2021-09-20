5edecf3b8aedf86d30ba0eadc6f8aaf0ca9dbc0d
DESKTOP-T0ARRTM\cwh78 <that8kguy@gmail.com>
2021-09-20 21:45:00 +0800

    Test cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,20 @@
-self.addEventListener("message", async (event) => {
-  console.log('Service Worker: ', event);
+var CACHE_HOME = "home-cache";
+var urlsToCache = ['/', '/promo'];
+
+self.addEventListener("install", async (event) => {
+  console.log("Caching");
+	event.waitUntil(
+		caches.open(CACHE_HOME).then((cache) => {
+			return cache.addAll(urlsToCache);
+		})
+	);
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log("fetching from cache");
+	event.respondWith(
+		caches.match(event.request).then((response) => {
+			return response || fetch(event.request);
+		})
+	);
 });
