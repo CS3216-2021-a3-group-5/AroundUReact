7d28bf39deeb2291a9b3df8cc2a578fc63ba109d
DESKTOP-T0ARRTM\cwh78 <that8kguy@gmail.com>
2021-09-20 23:27:51 +0800

    Restore service worker

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,8 +1,5 @@
 export default function ServiceWorkerRegistration() {
   if (process.env.NODE_ENV === 'production' && "serviceWorker" in navigator) {
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
-    if (publicUrl.origin !== window.location.origin) { return }
-
     const swPath = `${process.env.PUBLIC_URL}/service-worker.js`;
     window.addEventListener("load", function () {
       navigator.serviceWorker.register(swPath).then(() => {
