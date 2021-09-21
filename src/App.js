b86351ee76f7a002793d63e645eb118d7c3f92c4
DESKTOP-T0ARRTM\cwh78 <that8kguy@gmail.com>
2021-09-21 10:06:05 +0800

    Enable request geolocation

--- src/App.js
+++ src/App.js
@@ -14,51 +14,55 @@ import { createTheme } from "@material-ui/core/styles";
 import ScrollToTop from "./ScrollToTop";
 import ShareAdapter from "./Screens/Buyer/Share/ShareAdapter";
 import ServiceWorkerRegistration from "./serviceWorkerRegistration";
+import ImageSplashScreen from "./assets/Splashscreen_Lite.png"
 
 export default function App() {
 	if (!window.location.host.startsWith("localhost:3000") && window.location.protocol != "https:"){
 		window.location.protocol = "https";
 	}
 
-	// const [hasPermission, setPermission] = useState(false);
+	const [hasPermission, setPermission] = useState(false);
 
-	// navigator.permissions.query({ name: "geolocation" }).then((result) => {
-	// 	setPermission(result.state == "granted");
-	// });
+	navigator.permissions.query({ name: "geolocation" }).then((result) => {
+		setPermission(result.state == "granted");
+	});
 
-	// if (!hasPermission) {
-	// 	return (
-	// 		<div className="App">
-	// 			<img
-	// 				src={ImageSplashScreen}
-	// 				className="Image__request-background"
-	// 			/>
-	// 			<div className="Container__footer" style={{ borderWidth: 0 }}>
-	// 				<div className="Buffer__20px" />
-	// 				<div className="Container__large-screen-optimize Container__horizontal-padding-20px">
-	// 					<div
-	// 						className="Toggle__large--primary"
-	// 						onClick={() =>
-	// 							navigator.geolocation.getCurrentPosition(
-	// 								() => {
-	// 									setPermission(true);
-	// 								},
-	// 								() => {
-	// 									setPermission(false);
-	// 								}
-	// 							)
-	// 						}
-	// 					>
-	// 						<p className="Text__medium--light">
-	// 							Enable Location
-	// 						</p>
-	// 					</div>
-	// 				</div>
-	// 				<div className="Buffer__20px" />
-	// 			</div>
-	// 		</div>
-	// 	);
-	// }
+	console.log(hasPermission)
+
+	if (!hasPermission) {
+		return (
+			
+			<div className="App">
+				<img
+					src={ImageSplashScreen}
+					className="Image__request-background"
+				/>
+				<div className="Container__footer" style={{ borderWidth: 0 }}>
+					<div className="Buffer__20px" />
+					<div className="Container__large-screen-optimize Container__horizontal-padding-20px">
+						<div
+							className="Toggle__large--primary"
+							onClick={() =>
+								navigator.geolocation.getCurrentPosition(
+									() => {
+										setPermission(true);
+									},
+									() => {
+										setPermission(false);
+									}
+								)
+							}
+						>
+							<p className="Text__medium--light">
+								Enable Location
+							</p>
+						</div>
+					</div>
+					<div className="Buffer__20px" />
+				</div>
+			</div>
+		);
+	}
 
 	// Get service worker
 	ServiceWorkerRegistration()
